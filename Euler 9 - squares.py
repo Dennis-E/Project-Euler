bcb1ff95537849a5c3f7e5256d0b878f190fa3c2
Dennis-E <denniseggert@gmail.com>
2017-11-17 21:30:44 +0100

    Update and rename Euler9 - squares to Euler 9 - squares.py

--- Euler 9 - squares.py
+++ Euler 9 - squares.py
@@ -2,5 +2,3 @@ for a in range(999):
 	for b in range(a):
 		c=1000-a-b
 		if a**2+b**2==c**2: print(str(a)+" "+str(b)+" "+str(c))
-		
-#[(x,y,1000-x-y) for x in range(1,1000) for y in range(1,x) if x**2+y**2==(1000-x-y)**2]
