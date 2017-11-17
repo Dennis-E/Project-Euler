588ec099d20aaca79834e87dba2acf25a1dc846d
Dennis-E <denniseggert@gmail.com>
2017-11-17 21:25:07 +0100

    Update and rename Euler24.py to Euler 24.py

--- Euler 24.py
+++ Euler 24.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
- 
-This is a temporary script file.
 """
 counter=0
 
@@ -13,4 +11,4 @@ for i in range(1000000000,9876543210):
     if "0" in str(i) and "1" in str(i) and "2" in str(i) and"3" in str(i) and "4" in str(i) and "5" in str(i) and "6" in str(i) and "7" in str(i) and "8" in str(i) and "9" in str(i):
         counter+=1
         if counter==1000000: 
-is            print("The "+str(counter)+"solution is: "+str(i))
+            print("The "+str(counter)+"solution is: "+str(i))
